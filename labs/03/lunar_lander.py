ecda7334033641853c2ce97324f07d576303aebc
alexander <alexander@gazo.sk>
2020-10-24 16:33:06 +0200

    pass baseline

--- labs/03/lunar_lander.py
+++ labs/03/lunar_lander.py
@@ -12,35 +12,37 @@ parser.add_argument("--recodex", default=False, action="store_true", help="Runni
 parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
 parser.add_argument("--seed", default=None, type=int, help="Random seed.")
 # For these and any other arguments you add, ReCodEx will keep your default value.
-parser.add_argument("--alpha", default=None, type=float, help="Learning rate.")
-parser.add_argument("--epsilon", default=None, type=float, help="Exploration factor.")
-parser.add_argument("--gamma", default=None, type=float, help="Discounting factor.")
+parser.add_argument("--alpha", default=0.05, type=float, help="Learning rate.")
+parser.add_argument("--epsilon", default=0.1, type=float, help="Exploration factor.")
+parser.add_argument("--gamma", default=0.95, type=float, help="Discounting factor.")
+parser.add_argument("--q_path", default="Q_lunar.npy", type=str, help="Path to learned Q matrix.")
+parser.add_argument("--episodes", default=5000, type=int, help="Number of training episodes.")
+
 
 def main(env, args):
-    # Fix random seed
-    np.random.seed(args.seed)
+    if args.q_path is None:
+        np.random.seed(args.seed)
+        env.action_space.seed(args.seed)
 
-    # TODO: Implement a suitable RL algorithm.
+        Q = np.zeros((env.observation_space.n, env.action_space.n))
 
-    training = True
-    while training:
-        # To generate expert trajectory, you can use
-        state, trajectory = env.expert_trajectory()
+        for episode in range(args.episodes):
+            state, done = env.reset(), False
+            while not done:
+                if args.render_each and env.episode > 0 and env.episode % args.render_each == 0:
+                    env.render()
+                action = env.action_space.sample() if np.random.uniform() < args.epsilon else np.argmax(Q[state, :])
+                next_state, reward, done, _ = env.step(action)
+                Q[state, action] += args.alpha * (reward + args.gamma * np.max(Q[next_state, :]) - Q[state, action])
+                state = next_state
+        np.save("Q_lunar", Q)
+    else:
+        Q = np.load(args.q_path)
 
-        # TODO: Perform a training episode
-        state, done = env.reset(), False
-        while not done:
-            if args.render_each and env.episode and env.episode % args.render_each == 0:
-                env.render()
-
-            state, reward, done, _ = env.step(action)
-
-    # Final evaluation
     while True:
         state, done = env.reset(start_evaluation=True), False
         while not done:
-            # TODO: Choose (greedy) action
-            action = None
+            action = np.argmax(Q[state, :])
             state, reward, done, _ = env.step(action)
 
 
