6c299edfa13c4d31fcb0609d88c22fc766045a9f
alexander <alexander@gazo.sk>
2020-11-30 15:47:40 +0100

    solved

--- labs/07/paac.py
+++ labs/07/paac.py
@@ -19,46 +19,53 @@ parser.add_argument("--threads", default=1, type=int, help="Maximum number of th
 parser.add_argument("--env", default="CartPole-v1", type=str, help="Environment.")
 parser.add_argument("--evaluate_each", default=100, type=int, help="Evaluate each number of batches.")
 parser.add_argument("--evaluate_for", default=10, type=int, help="Evaluate the given number of episodes.")
-parser.add_argument("--gamma", default=None, type=float, help="Discounting factor.")
-parser.add_argument("--hidden_layer_size", default=None, type=int, help="Size of hidden layer.")
-parser.add_argument("--learning_rate", default=None, type=float, help="Learning rate.")
-parser.add_argument("--workers", default=None, type=int, help="Number of parallel workers.")
+parser.add_argument("--gamma", default=0.99, type=float, help="Discounting factor.")
+parser.add_argument("--hidden_layer_size", default=50, type=int, help="Size of hidden layer.")
+parser.add_argument("--learning_rate", default=0.002, type=float, help="Learning rate.")
+parser.add_argument("--workers", default=128, type=int, help="Number of parallel workers.")
+parser.add_argument("--pass_limit", default=450, type=int, help="Stop evaluation after reaching.")
 
 class Network:
-    def __init__(self, env, args):
-        # TODO: Similarly to reinforce with baseline, define two components:
-        # - actor, which predicts distribution over the actions
-        # - critic, which predicts the value function
-        #
-        # Use independent networks for both of them, each with
-        # `args.hidden_layer_size` neurons in one ReLU hidden layer,
-        # and train them using Adam with given `args.learning_rate`.
-        raise NotImplementedError()
+    def __init__(self, env, args, actor=None, critic=None):
+        if actor is None:
+            actor = tf.keras.Sequential()
+            actor.add(tf.keras.Input(shape=env.observation_space.shape))
+            actor.add(tf.keras.layers.Dense(args.hidden_layer_size, activation='relu'))
+            actor.add(tf.keras.layers.Dense(env.action_space.n, activation='softmax'))
+            actor.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(), optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate))
+        if critic is None:
+            critic = tf.keras.Sequential()
+            critic.add(tf.keras.Input(shape=env.observation_space.shape))
+            critic.add(tf.keras.layers.Dense(args.hidden_layer_size, activation='relu'))
+            critic.add(tf.keras.layers.Dense(1))
+            critic.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate))
+        self._actor = actor
+        self._critic = critic
 
-    # The `wrappers.typed_np_function` automatically converts input arguments
-    # to NumPy arrays of given type, and converts the result to a NumPy array.
     @wrappers.typed_np_function(np.float32, np.int32, np.float32)
     @tf.function
     def train(self, states, actions, returns):
-        # TODO: Train the policy network using policy gradient theorem
-        # and the value network using MSE.
-        raise NotImplementedError()
+        delta = returns - tf.squeeze(self._critic(states, training=True))
+        self._actor.optimizer.minimize(
+            lambda: self._actor.loss(actions, self._actor(states, training=True), sample_weight=delta),
+            var_list=self._actor.trainable_variables)
+        self._critic.optimizer.minimize(
+            lambda: self._critic.loss(returns, tf.squeeze(self._critic(states, training=True))),
+            var_list=self._critic.trainable_variables)
 
     @wrappers.typed_np_function(np.float32)
     @tf.function
     def predict_actions(self, states):
-        # TODO: Return predicted action probabilities.
-        raise NotImplementedError()
+        return self._actor(states)
 
     @wrappers.typed_np_function(np.float32)
     @tf.function
     def predict_values(self, states):
-        # TODO: Return estimates of value function.
-        raise NotImplementedError()
+        return self._critic(states)
 
 def main(env, args):
     # Fix random seeds and number of threads
-    np.random.seed(args.seed)
+    rng = np.random.default_rng(args.seed)
     tf.random.set_seed(args.seed)
     tf.config.threading.set_inter_op_parallelism_threads(args.threads)
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
@@ -72,8 +79,7 @@ def main(env, args):
             if args.render_each and env.episode > 0 and env.episode % args.render_each == 0:
                 env.render()
 
-            # TODO: Predict the action using the greedy policy
-            action = None
+            action = np.argmax(network.predict_actions([state])[0])
             state, reward, done, _ = env.step(action)
             rewards += reward
         return rewards
@@ -84,20 +90,23 @@ def main(env, args):
 
     training = True
     while training:
-        # Training
         for _ in range(args.evaluate_each):
-            # TODO: Choose actions using network.predict_actions
-            actions = None
+            actions = [rng.choice(env.action_space.n, p=p) for p in network.predict_actions(states)]
 
-            # TODO: Perform steps in the vectorized environment
+            next_states, rewards, dones, _ = vector_env.step(actions)
 
-            # TODO: Compute estimates of returns by one-step bootstrapping
+            returns = rewards + args.gamma * (~dones) * network.predict_values(next_states).flatten()
 
-            # TODO: Train network using current states, chosen actions and estimated returns
+            network.train(states, actions, returns)
+
+            states = next_states
 
         # Periodic evaluation
         for _ in range(args.evaluate_for):
             evaluate_episode()
+
+        if np.mean(env._episode_returns[-args.evaluate_for:]) > args.pass_limit:
+            training = False
 
     # Final evaluation
     while True:
