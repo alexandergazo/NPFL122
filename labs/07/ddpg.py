f48dd485f9795bd0bd6380a672425c29086c2a63
alexander <alexander@gazo.sk>
2020-12-02 23:11:41 +0100

    solved

--- labs/07/ddpg.py
+++ labs/07/ddpg.py
@@ -17,61 +17,84 @@ parser.add_argument("--render_each", default=0, type=int, help="Render some epis
 parser.add_argument("--seed", default=None, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
 # For these and any other arguments you add, ReCodEx will keep your default value.
-parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
+parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
 parser.add_argument("--env", default="Pendulum-v0", type=str, help="Environment.")
-parser.add_argument("--evaluate_each", default=50, type=int, help="Evaluate each number of episodes.")
-parser.add_argument("--evaluate_for", default=50, type=int, help="Evaluate the given number of episodes.")
-parser.add_argument("--gamma", default=None, type=float, help="Discounting factor.")
-parser.add_argument("--hidden_layer_size", default=None, type=int, help="Size of hidden layer.")
-parser.add_argument("--learning_rate", default=None, type=float, help="Learning rate.")
+parser.add_argument("--evaluate_each", default=100, type=int, help="Evaluate each number of episodes.")
+parser.add_argument("--evaluate_for", default=100, type=int, help="Evaluate the given number of episodes.")
+parser.add_argument("--gamma", default=0.99, type=float, help="Discounting factor.")
+parser.add_argument("--hidden_layer_size", default=50, type=int, help="Size of hidden layer.")
+parser.add_argument("--learning_rate", default=0.002, type=float, help="Learning rate.")
 parser.add_argument("--noise_sigma", default=0.2, type=float, help="UB noise sigma.")
 parser.add_argument("--noise_theta", default=0.15, type=float, help="UB noise theta.")
-parser.add_argument("--target_tau", default=None, type=float, help="Target network update weight.")
+parser.add_argument("--target_tau", default=0.01, type=float, help="Target network update weight.")
+parser.add_argument("--pass_limit", default=-200, type=int, help="Stop evaluation after reaching.")
+
+
+def get_scaled_tanh(low=-1, high=1):
+    from tensorflow.keras.activations import sigmoid, tanh
+    if low==-1 and high==1:
+        return tanh
+    else:
+        return lambda x: sigmoid(2 * x) * (high - low) + low
 
 class Network:
     def __init__(self, env, args):
-        # TODO: Create:
-        # - an actor, which starts with states and returns actions.
-        #   Usually, one or two hidden layers are employed. As in the
-        #   paac_continuous, to keep the actions in the required range, you
-        #   should apply properly scaled `tf.tanh` activation.
-        #
-        # - a target actor as the copy of the actor using `tf.keras.models.clone_model`.
-        #
-        # - a critic, starting with given states and actions producing predicted
-        #   returns.  Usually, states are fed through a hidden layer first, and
-        #   then concatenated with action and fed through two more hidden
-        #   layers, before computing the returns.
-        #
-        # - a target critic as the copy of the critic using `tf.keras.models.clone_model`.
-        raise NotImplementedError()
+        self.target_tau = args.target_tau
+        low, high = env.action_space.low, env.action_space.high
+
+        inputs = tf.keras.Input(shape=env.observation_space.shape)
+        actor = tf.keras.layers.Dense(args.hidden_layer_size, activation='relu')(inputs)
+        actor = tf.keras.layers.Dense(args.hidden_layer_size, activation='relu')(actor)
+        actor = tf.keras.layers.Dense(env.action_space.shape[0])(actor)
+        actor = tf.keras.layers.Lambda(get_scaled_tanh(low, high))(actor)
+        self.actor = tf.keras.Model(inputs=inputs, outputs=actor)
+        self.actor.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate / 10))
+        self.target_actor = tf.keras.models.clone_model(self.actor)
+
+        critic = tf.keras.layers.Dense(args.hidden_layer_size, activation='relu')(inputs)
+        action_inputs = tf.keras.Input(shape=env.action_space.shape)
+        critic = tf.keras.layers.concatenate([critic, action_inputs])
+        critic = tf.keras.layers.Dense(args.hidden_layer_size, activation='relu')(critic)
+        critic = tf.keras.layers.Dense(args.hidden_layer_size, activation='relu')(critic)
+        critic = tf.keras.layers.Dense(1)(critic)
+        self.critic = tf.keras.Model(inputs=[inputs, action_inputs], outputs=critic)
+        self.critic.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate),
+                            loss=tf.keras.losses.MeanSquaredError())
+        self.target_critic = tf.keras.models.clone_model(self.critic)
+        # tf.keras.utils.plot_model(self.critic, show_shapes=True)
 
     @wrappers.typed_np_function(np.float32, np.float32, np.float32)
     @tf.function
     def train(self, states, actions, returns):
-        # TODO: Separately train:
-        # - the actor using the DPG loss,
-        # - the critic using MSE loss.
-        #
-        # Furthermore, update the target actor and critic networks by
-        # exponential moving average with weight `args.target_tau`. A possible
-        # way to implement it inside a `tf.function` is the following:
-        #   for var, target_var in zip(network.trainable_variables, target_network.trainable_variables):
-        #       target_var.assign(target_var * (1 - target_tau) + var * target_tau)
-        raise NotImplementedError()
+        self.actor.optimizer.minimize(
+            lambda: -self.critic([states, self.actor(states, training=True)], training=True),
+            var_list=self.actor.trainable_variables
+        )
+
+        self.critic.optimizer.minimize(
+            lambda: self.critic.loss(returns, self.critic([states, actions], training=True)),
+            var_list=self.critic.trainable_variables
+        )
+
+        for var, target_var in zip(self.critic.trainable_variables, self.target_critic.trainable_variables):
+            target_var.assign(target_var * (1 - self.target_tau) + var * self.target_tau)
+        for var, target_var in zip(self.actor.trainable_variables, self.target_actor.trainable_variables):
+            target_var.assign(target_var * (1 - self.target_tau) + var * self.target_tau)
 
     @wrappers.typed_np_function(np.float32)
     @tf.function
     def predict_actions(self, states):
-        # TODO: Return predicted actions by the actor.
-        raise NotImplementedError()
+        return self.actor(states)
+
+    @wrappers.typed_np_function(np.float32)
+    @tf.function
+    def predict_target_actions(self, states):
+        return self.target_actor(states)
 
     @wrappers.typed_np_function(np.float32)
     @tf.function
     def predict_values(self, states):
-        # TODO: Return predicted returns -- predict actions by the target actor
-        # and evaluate them using the target critic.
-        raise NotImplementedError()
+        return self.target_critic([states, self.target_actor(states)])
 
 
 class OrnsteinUhlenbeckNoise:
@@ -112,8 +135,7 @@ def main(env, args):
             if args.render_each and env.episode > 0 and env.episode % args.render_each == 0:
                 env.render()
 
-            # TODO: Predict the action using the greedy policy
-            action = None
+            action = network.predict_target_actions([state])[0]
             state, reward, done, _ = env.step(action)
             rewards += reward
         return rewards
@@ -121,15 +143,12 @@ def main(env, args):
     noise = OrnsteinUhlenbeckNoise(env.action_space.shape[0], 0, args.noise_theta, args.noise_sigma)
     training = True
     while training:
-        # Training
         for _ in range(args.evaluate_each):
             state, done = env.reset(), False
             noise.reset()
             while not done:
-                # TODO: Predict actions by calling `network.predict_actions`
-                # and adding the Ornstein-Uhlenbeck noise. As in paac_continuous,
-                # clip the actions to the `env.action_space.{low,high}` range.
-                action = None
+                action = network.predict_actions([state])[0] + noise.sample()
+                action = action.clip(env.action_space.low, env.action_space.high)
 
                 next_state, reward, done, _ = env.step(action)
                 replay_buffer.append(Transition(state, action, reward, done, next_state))
@@ -138,11 +157,15 @@ def main(env, args):
                 if len(replay_buffer) >= args.batch_size:
                     batch = np.random.choice(len(replay_buffer), size=args.batch_size, replace=False)
                     states, actions, rewards, dones, next_states = map(np.array, zip(*[replay_buffer[i] for i in batch]))
-                    # TODO: Perform the training
+                    returns = rewards + args.gamma * (~dones) * network.predict_values(next_states).flatten()
+                    network.train(states, actions, returns.reshape(-1, 1))
 
         # Periodic evaluation
         for _ in range(args.evaluate_for):
             evaluate_episode()
+
+        if np.mean(env._episode_returns[-args.evaluate_for:]) > args.pass_limit:
+            training = False
 
     # Final evaluation
     while True:
