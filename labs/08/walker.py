a6b2fdf2fe0d343672adba486d71f98e06adf54c
alexander <alexander@gazo.sk>
2020-12-06 11:56:44 +0100

    implement td3

--- labs/08/walker.py
+++ labs/08/walker.py
@@ -15,31 +15,202 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
 parser.add_argument("--seed", default=None, type=int, help="Random seed.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--threads", default=4, type=int, help="Maximum number of threads to use.")
 # For these and any other arguments you add, ReCodEx will keep your default value.
+parser.add_argument("--batch_size", default=100, type=int, help="Batch size.")
+parser.add_argument("--evaluate_each", default=100, type=int, help="Evaluate each number of episodes.")
+parser.add_argument("--evaluate_for", default=100, type=int, help="Evaluate the given number of episodes.")
+parser.add_argument("--gamma", default=0.99, type=float, help="Discounting factor.")
+parser.add_argument("--hidden_layer_size", default=100, type=int, help="Size of hidden layer.")
+parser.add_argument("--learning_rate", default=0.001, type=float, help="Learning rate.")
+parser.add_argument("--noise_sigma", default=0.05, type=float, help="UB noise sigma.")
+parser.add_argument("--noise_theta", default=0.15, type=float, help="UB noise theta.")
+parser.add_argument("--target_tau", default=0.005, type=float, help="Target network update weight.")
+parser.add_argument("--pass_limit", default=100, type=int, help="Stop evaluation after reaching.")
+parser.add_argument("--max_buffer_size", default=10000, type=int, help="Buffer size limit.")
+parser.add_argument("--delay_freq", default=2, type=int, help="Delay parameter.")
+parser.add_argument("--policy_noise_clip", default=0.05, type=float, help='Policy smoothing clip.')
+parser.add_argument("--policy_noise_sigma", default=0.05, type=float, help='Policy smoothing sigma.')
+
+
+def get_scaled_tanh(low=-1, high=1):
+    from tensorflow.keras.activations import sigmoid, tanh
+    if low==-1 and high==1:
+        return tanh
+    else:
+        return lambda x: sigmoid(2 * x) * (high - low) + low
+
+class Network:
+    def __init__(self, env, args, rng):
+        self.target_tau = args.target_tau
+        self.rng = rng
+        self.c = args.policy_noise_clip
+        self.sigma = args.policy_noise_sigma
+
+        if np.unique(env.action_space.low).shape[0] == 1 and np.unique(
+            env.action_space.high).shape[0] == 1:
+            low = np.unique(env.action_space.low)[0]
+            high = np.unique(env.action_space.high)[0]
+        else:
+            raise NotImplementedError()
+
+        inputs = tf.keras.Input(shape=env.observation_space.shape)
+
+        actor = tf.keras.layers.Dense(args.hidden_layer_size, activation='relu')(inputs)
+        actor = tf.keras.layers.Dense(args.hidden_layer_size, activation='relu')(actor)
+        actor = tf.keras.layers.Dense(env.action_space.shape[0])(actor)
+        actor = tf.keras.layers.Lambda(get_scaled_tanh(low, high))(actor)
+        self.actor = tf.keras.Model(inputs=inputs, outputs=actor)
+        self.actor.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate))
+
+        critic = tf.keras.layers.Dense(args.hidden_layer_size, activation='relu')(inputs)
+        action_inputs = tf.keras.Input(shape=env.action_space.shape)
+        critic = tf.keras.layers.concatenate([critic, action_inputs])
+        critic = tf.keras.layers.Dense(args.hidden_layer_size, activation='relu')(critic)
+        critic = tf.keras.layers.Dense(args.hidden_layer_size, activation='relu')(critic)
+        critic = tf.keras.layers.Dense(1)(critic)
+        self.critic = tf.keras.Model(inputs=[inputs, action_inputs], outputs=critic)
+        self.critic2 = tf.keras.models.clone_model(self.actor)
+        self.critic.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate),
+                            loss=tf.keras.losses.MeanSquaredError())
+        self.critic2.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate),
+                             loss=tf.keras.losses.MeanSquaredError())
+
+        self.target_actor = tf.keras.models.clone_model(self.actor)
+        self.target_critic = tf.keras.models.clone_model(self.critic)
+        self.target_critic2 = tf.keras.models.clone_model(self.critic2)
+
+
+    # the functions are split due to unresolved bug in tensorflow #34983
+    @wrappers.typed_np_function(np.float32, np.float32, np.float32)
+    @tf.function
+    def train_critics(self, states, actions, returns):
+        self.critic.optimizer.minimize(
+            lambda: self.critic.loss(returns, self.critic([states, actions], training=True)),
+            var_list=self.critic.trainable_variables
+        )
+        self.critic2.optimizer.minimize(
+            lambda: self.critic2.loss(returns, self.critic2([states, actions], training=True)),
+            var_list=self.critic2.trainable_variables
+        )
+
+
+    @wrappers.typed_np_function(np.float32, np.float32, np.float32)
+    @tf.function
+    def train_actor_and_targets(self, states, actions, returns):
+        self.actor.optimizer.minimize(
+            lambda: -self.critic([states, self.actor(states, training=True)], training=True),
+            var_list=self.actor.trainable_variables
+        )
+
+        for var, target_var in zip(self.critic.trainable_variables, self.target_critic.trainable_variables):
+            target_var.assign(target_var * (1 - self.target_tau) + var * self.target_tau)
+        for var, target_var in zip(self.actor.trainable_variables, self.target_actor.trainable_variables):
+            target_var.assign(target_var * (1 - self.target_tau) + var * self.target_tau)
+
+
+    @wrappers.typed_np_function(np.float32)
+    @tf.function
+    def predict_actions(self, states):
+        return self.actor(states)
+
+
+    @wrappers.typed_np_function(np.float32)
+    @tf.function
+    def predict_target_actions(self, states):
+        return self.target_actor(states)
+
+
+    @wrappers.typed_np_function(np.float32)
+    @tf.function
+    def predict_values(self, states):
+        actor = self.target_actor(states)
+        noise = np.clip(self.rng.normal(scale=self.sigma, size=actor.shape), -self.c, self.c)
+        critic = self.target_critic([states, actor + noise])
+        critic2 = self.target_critic([states, actor + noise])
+        return tf.minimum(critic, critic2)
+
+
+class OrnsteinUhlenbeckNoise:
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, shape, mu, theta, sigma, rng):
+        self.mu = mu * np.ones(shape)
+        self.theta = theta
+        self.sigma = sigma
+        self.rng = rng
+        self.reset()
+
+    def reset(self):
+        self.state = np.copy(self.mu)
+
+    def sample(self):
+        self.state += self.theta * (self.mu - self.state) + self.rng.normal(scale=self.sigma, size=self.state.shape)
+        return self.state
+
 
 def main(env, args):
     # Fix random seeds and number of threads
-    np.random.seed(args.seed)
+    rng = np.random.default_rng(args.seed)
     tf.random.set_seed(args.seed)
     tf.config.threading.set_inter_op_parallelism_threads(args.threads)
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
-    if args.recodex:
-        # TODO: Perform evaluation of a trained model.
+    network = Network(env, args, rng)
 
-        while True:
-            state, done = env.reset(start_evaluation=True), False
+    replay_buffer = collections.deque(maxlen=args.max_buffer_size)
+    Transition = collections.namedtuple("Transition", ["state", "action", "reward", "done", "next_state"])
+
+    def evaluate_episode(start_evaluation=False):
+        rewards, state, done = 0, env.reset(start_evaluation), False
+        while not done:
+            if args.render_each and env.episode > 0 and env.episode % args.render_each == 0:
+                env.render()
+
+            action = network.predict_target_actions([state])[0]
+            state, reward, done, _ = env.step(action)
+            rewards += reward
+        return rewards
+
+    noise = OrnsteinUhlenbeckNoise(env.action_space.shape[0], 0, args.noise_theta, args.noise_sigma, rng)
+    training = True
+    while training:
+        for _ in range(args.evaluate_each):
+            state, done = env.reset(), False
+            timestep = 0
+            noise.reset()
             while not done:
-                # TODO: Choose an action
-                action = None
+                action = network.predict_actions([state])[0] + rng.normal(scale=args.noise_sigma)
+                action = action.clip(env.action_space.low, env.action_space.high)
 
-                state, reward, done, _ = env.step(action)
+                next_state, reward, done, _ = env.step(action)
+                replay_buffer.append(Transition(state, action, reward, done, next_state))
+                state = next_state
 
-    else:
-        # TODO: Perform training
+                if len(replay_buffer) >= args.batch_size:
+                    batch = rng.choice(len(replay_buffer), size=args.batch_size, replace=False)
+                    states, actions, rewards, dones, next_states = map(np.array, zip(*[replay_buffer[i] for i in batch]))
+                    returns = rewards + args.gamma * (~dones) * network.predict_values(next_states).flatten()
+                    network.train_critics(states, actions, returns.reshape(-1, 1))
 
-        pass
+                    if timestep % args.delay_freq == 0:
+                        network.train_actor_and_targets(states, actions, returns.reshape(-1, 1))
+
+                timestep += 1
+
+        # Periodic evaluation
+        for _ in range(args.evaluate_for):
+            evaluate_episode()
+
+        if np.mean(env._episode_returns[-args.evaluate_for:]) > args.pass_limit:
+            training = False
+
+    import pickle, lzma
+    with lzma.open('walker.model', 'wb') as model_file:
+        pickle.dump(network, model_file)
+    # Final evaluation
+    while True:
+        evaluate_episode(start_evaluation=True)
 
 
 if __name__ == "__main__":
